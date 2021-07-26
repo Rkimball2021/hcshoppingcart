e0f710e8a485fde7058f2bff878ad94118ff481f
Ruth Kimball <86669796+Rkimball2021@users.noreply.github.com>
2021-07-25 21:19:39 -0300

    Update style.ts

--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -8,6 +8,8 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    margin left: 60px;
+    margin right:60px;
 
     .cart{
       display: flex;
